15e998f8e98ada86a085e0aa7f3fc66ef42191d0
46989414t <46989414t@iespoblenou.org>
2016-02-24 20:42:00 +0100

    Initial commit

---
+++ app/src/main/java/com/projecteuf2/projecteuf2/BlankFragment.java
@@ -0,0 +1,31 @@
+package com.projecteuf2.projecteuf2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.blank_fragment_layout, container, false);
+
+
+        return vista;
+    }
+
+}
