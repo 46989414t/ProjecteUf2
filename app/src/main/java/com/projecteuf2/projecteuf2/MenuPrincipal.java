da7f2a14da680481f7b65691b4ce16d30fe1c452
46989414t <enric@Mac-mini-de-Enric.local>
2016-02-28 13:30:06 +0100

    popUp para seleccionar camara, galeria

--- app/src/main/java/com/projecteuf2/projecteuf2/MenuPrincipal.java
+++ app/src/main/java/com/projecteuf2/projecteuf2/MenuPrincipal.java
@@ -1,24 +1,31 @@
 package com.projecteuf2.projecteuf2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,6 +42,14 @@ public class MenuPrincipal extends AppCompatActivity
 
     String mCurrentPhotoPath;
 
+    private String APP_DIRECTORY = "photoApp/";
+    private String MEDIA_DIRECTORY =APP_DIRECTORY+"media/";
+    private String TEMPORAL_NAME = "temporal.jpg";
+
+    private final int PHOTO_CODE = 100;
+    private final int SELECT_PICTURE = 200;
+
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,16 +60,6 @@ public class MenuPrincipal extends AppCompatActivity
         Firebase.setAndroidContext(this);
         Firebase myFirebase = new Firebase("https://pruebaparafirebase.firebaseio.com/");
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -63,6 +68,41 @@ public class MenuPrincipal extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        imageView = (ImageView) findViewById(R.id.imageView2);
+
+        myFirebase.child("photo_profile").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("DATOS_PERFIL: " + dataSnapshot);
+                System.out.println("VALOR: " + dataSnapshot.getValue());
+                // Bitmap fotoPerfil = (Bitmap) dataSnapshot.getValue();
+                /*String fotoPerfil = dataSnapshot.getValue().toString();
+                Bitmap fotografia = StringToBitMap(fotoPerfil);
+                imageView.setImageBitmap(fotografia);*/
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+
+
+
+    }
+
+    private Bitmap StringToBitMap(String encodedString) {
+        try{
+            byte [] encodeByte=Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap=BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        }catch(Exception e){
+            e.getMessage();
+            return null;
+        }
     }
 
     @Override
@@ -140,8 +180,27 @@ public class MenuPrincipal extends AppCompatActivity
     }
 
     public void onToFotoPerfil(View view) throws IOException {
-        Firebase myFirebase = new Firebase("https://pruebaparafirebase.firebaseio.com/");
-        dispatchTakePictureIntent();
+       /* Firebase myFirebase = new Firebase("https://pruebaparafirebase.firebaseio.com/");
+        dispatchTakePictureIntent();*/
+
+        final CharSequence[] options = {"Tomar foto", "Elegir de galería", "Cancelar"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MenuPrincipal.this);
+        builder.setTitle("Elige una opcion");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int seleccion) {
+                if (options[seleccion] == "Tomar foto") {
+                    openCamera();
+                } else if (options[seleccion] == "Elegir de galería") {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*"); //muesra todas las imagenes
+                    startActivityForResult(intent.createChooser(intent, "Selecciona app de la imagen"), SELECT_PICTURE);
+                } else if (options[seleccion] == "Cancelar") {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
 
         /*
 
@@ -182,7 +241,51 @@ public class MenuPrincipal extends AppCompatActivity
 */
         }
 
-    /*private String getCode() {
+    private void openCamera() {
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), MEDIA_DIRECTORY);
+        file.mkdirs();//crear directorio donde se guardara la imagen
+
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) +File.separator + MEDIA_DIRECTORY +
+                File.separator + TEMPORAL_NAME;
+
+        File newFile = new File(path);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
+        startActivityForResult(intent, PHOTO_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case PHOTO_CODE:
+                if(resultCode == RESULT_OK){
+                    String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+File.separator +
+                            MEDIA_DIRECTORY + File.separator + TEMPORAL_NAME;
+                   // decodeBitmap(dir); //FALLA
+                }
+                break;
+            case SELECT_PICTURE:
+                if(resultCode==RESULT_OK){
+                    Uri path = data.getData();
+                    //imageView.setImageURI(path); FALLA
+                }
+        }
+    }
+
+    private void decodeBitmap(String dir) {
+        System.out.println("GUARDA LA FOTO EN FIRABASE");
+        Firebase myFirebase = new Firebase("https://pruebaparafirebase.firebaseio.com/");
+
+        Bitmap bitmap;
+        bitmap = BitmapFactory.decodeFile(dir);
+
+      //  imageView.setImageBitmap(bitmap);  FALLA
+
+        myFirebase.child("photo_profile").setValue(bitmap);
+    }
+/*private String getCode() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
         String date = dateFormat.format(new Date());
         String photoCode = "pic_" + date;
